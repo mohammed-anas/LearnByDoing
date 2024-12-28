051c73e82cd1b5071f6df312c907e73227959179
Mohammed Anas <anas.wilshire@gmail.com>
2024-12-28 17:39:25 +0400

    first commit

---
+++ src/main/java/com/example/learnbydoing/LearnByDoingApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnbydoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnByDoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnByDoingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learnbydoing/LearnByDoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnbydoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnByDoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
